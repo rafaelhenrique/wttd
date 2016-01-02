bb789175ce8ea80ee9a229c77c39989868269b28
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-01-02 13:57:13 -0200

    Adds Django form

--- eventex/subscriptions/tests.py
+++ eventex/subscriptions/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from eventex.subscriptions.forms import SubscriptionForm
 
 
 class SubscribeTest(TestCase):
@@ -18,7 +19,22 @@ class SubscribeTest(TestCase):
     def test_html(self):
         """Html must contain input tags"""
         self.assertContains(self.resp, '<form')
-        self.assertContains(self.resp, '<input', 5)
+        self.assertContains(self.resp, '<input', 6)
         self.assertContains(self.resp, 'type="text"', 3)
         self.assertContains(self.resp, 'type="email"')
         self.assertContains(self.resp, 'type="submit"')
+
+    def test_csrf(self):
+        """Html must contain csrf"""
+        self.assertContains(self.resp, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        """Context must have subscription form"""
+        form = self.resp.context['form']
+        self.assertIsInstance(form, SubscriptionForm)
+
+    def test_form_has_fields(self):
+        """Form must have 4 fields"""
+        form = self.resp.context['form']
+        self.assertSequenceEqual(
+            ['name', 'cpf', 'email', 'phone'], list(form.fields))
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from eventex.subscriptions.forms import SubscriptionForm
+
 
 def subscribe(request):
-    return render(request, 'subscriptions/subscription_form.html')
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscriptions/subscription_form.html', context)
