85c702631923cd3b47ec992c4cfcb06d9f172827
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-01-14 22:33:19 -0200

    Phone or email optionals

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -14,8 +14,8 @@ class SubscriptionForm(forms.Form):
     name = forms.CharField(label='Nome')
     cpf = forms.CharField(label='CPF',
                           validators=[validate_cpf])
-    email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
+    email = forms.EmailField(label='Email', required=False)
+    phone = forms.CharField(label='Telefone', required=False)
 
     def clean_name(self):
         name = self.cleaned_data['name']
@@ -26,3 +26,9 @@ class SubscriptionForm(forms.Form):
             else:
                 words.append(w)
         return ' '.join(words)
+
+    def clean(self):
+        if ((not self.cleaned_data.get('email') and
+             not self.cleaned_data.get('phone'))):
+            raise ValidationError('Informe seu e-mail ou telefone.')
+        return self.cleaned_data
--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -25,6 +25,21 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(name='RAFAEL correia')
         self.assertEqual('Rafael Correia', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Email is optional"""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed"""
+        form = self.make_validated_form(email='', phone='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
