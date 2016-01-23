93c6d5bfd34da1a4d173e580be4eb293fc28d1e4
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-01-23 16:54:28 -0200

    Adding Speaker details and add speakers on admin

---
+++ eventex/core/tests/test_model_speaker.py
@@ -0,0 +1,33 @@
+from eventex.core.models import Speaker
+
+
+from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
+
+class SpeakerModelTest(TestCase):
+    def setUp(self):
+        self.speaker = Speaker.objects.create(
+            name='Grace Hopper',
+            slug='grace-hopper',
+            photo='http://hbn.link/hopper-pic',
+            website='http://hbn.link/hopper-site',
+            description='Programadora e almirante.',
+        )
+
+    def test_create(self):
+        self.assertTrue(Speaker.objects.exists())
+
+    def test_fields_can_be_blank(self):
+        fields = ('description', 'website')
+        for field in fields:
+            with self.subTest():
+                field = Speaker._meta.get_field(field)
+                self.assertTrue(field.blank)
+
+    def test_str(self):
+        self.assertEqual('Grace Hopper', str(self.speaker))
+
+    def test_get_absolute_url(self):
+        url = r('speaker_detail', slug=self.speaker.slug)
+        self.assertEqual(url, self.speaker.get_absolute_url())
