569e3474bbbeefde245cada6437185986ccb2d07
Rafael Henrique da Silva Correia <rafael@abraseucodigo.com.br>
2016-01-22 22:21:08 -0200

    Removing old form

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,32 +1,7 @@
 from eventex.subscriptions.models import Subscription
-from eventex.subscriptions.validators import validate_cpf
 
 from django import forms
 from django.core.exceptions import ValidationError
-
-
-class SubscriptionFormOld(forms.Form):
-    name = forms.CharField(label='Nome')
-    cpf = forms.CharField(label='CPF',
-                          validators=[validate_cpf])
-    email = forms.EmailField(label='Email', required=False)
-    phone = forms.CharField(label='Telefone', required=False)
-
-    def clean_name(self):
-        name = self.cleaned_data['name']
-        words = []
-        for w in name.split():
-            if len(w) > 2:
-                words.append(w.capitalize())
-            else:
-                words.append(w)
-        return ' '.join(words)
-
-    def clean(self):
-        if ((not self.cleaned_data.get('email') and
-             not self.cleaned_data.get('phone'))):
-            raise ValidationError('Informe seu e-mail ou telefone.')
-        return self.cleaned_data
 
 
 class SubscriptionForm(forms.ModelForm):
