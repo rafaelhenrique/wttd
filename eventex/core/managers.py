e627d357922791d385ebe611711f5acde2489300
Rafael Correia <rafael.correia@luizalabs.com>
2016-02-10 14:12:36 -0200

    Adding PeriodManager

--- eventex/core/managers.py
+++ eventex/core/managers.py
@@ -7,3 +7,13 @@ class KindQuerySet(models.QuerySet):
 
     def phones(self):
         return self.filter(kind=self.model.PHONE)
+
+
+class PeriodManager(models.Manager):
+    MIDDAY = '12:00'
+
+    def at_morning(self):
+        return self.filter(start__lt=self.MIDDAY)
+
+    def at_afternoon(self):
+        return self.filter(start__gte=self.MIDDAY)
